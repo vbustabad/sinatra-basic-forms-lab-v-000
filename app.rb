d9410635abc186e4c3d0ac6546073d67d4fd69c3
Veronica Bustabad <vbustabad@gmail.com>
2018-07-11 03:50:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
+require 'pry'
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    new_name = params[:name]
+    new_breed = params[:breed]
+    new_age = params[:age]
+    @puppy_information = Puppy.new(new_name, new_breed, new_age)
+
+    erb :display_puppy
+  end
 
 end
